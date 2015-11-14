0791db316b8dd96575c13839ab00d0c57240e5f6
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-11-14 00:15:45 +0000

    CustomError(Exception)

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 2 module"""
+
+
+class CustomError(Exception):
+    """CustomError Class"""
+    def __init__(self, cause):
+        """Class constructor"""
+        Exception.__init__(self, cause):
+        self.cause = cause
