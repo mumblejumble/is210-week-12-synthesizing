a5902c9cbb1af15553eb440fcc3a2ed3eccc89dc
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-11-14 00:16:41 +0000

    Exception handling

--- task_03.py
+++ task_03.py
@@ -2,12 +2,29 @@
 # -*- coding: utf-8 -*-
 """Task 03 module"""
 
+
 def exception_test(arg1, arg2, arg3):
+    """Exception test function that catches specific erros
+
+    Args:
+        arg1(mixed): first argument
+        arg2(mixed): second argument
+        arg3(mixed): third argument
+
+    Examples:
+        >>> exception_test(['apple'], 0, 'p')
+        False
+        >>> exception_test(43, 1, 1)
+        True
+        >>> exception_test(['apple'], 0, x)
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        NameError: name 'x' is not defined
+    """
     caught = False
     try:
         arg1[arg2].index(arg3)
-    except:
+    except (TypeError, KeyError, IndexError):
         caught = True
 
     return caught
-    
